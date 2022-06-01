aed2bf3d21b17b2a4d70555385a7e59467733b94
Kaliabianau, Vitali <vitali.kaliabianau@ihsmarkit.com>
2022-06-01 20:49:24 +0300

    Add fake `ProductService`

---
+++ Diploma/ClientApp/src/services/ProductService.js
@@ -0,0 +1,30 @@
+import s123 from '@/assets/s123.png'
+
+const products = [
+  {
+    id: '1',
+    image: s123,
+    name: 'Букет',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo autem ut.',
+    cost: 25
+  },
+  {
+    id: '2',
+    image: s123,
+    name: 'Букет 2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo autem ut.',
+    cost: 30
+  }
+]
+
+class ProductService {
+  async getProducts () {
+    return products
+  }
+
+  async getProductById (id) {
+    return products.filter(x => x.id === id)[0]
+  }
+}
+
+export default new ProductService()
